baaa9455b978386c4ddda9404bbb514d52f07c31
Cedric <128242507+Cedrix-code@users.noreply.github.com>
2024-11-11 05:19:33 +0800

    updated webhooks to convex

--- convex/schema.ts
+++ convex/schema.ts
@@ -5,5 +5,8 @@ export default defineSchema({
   files: defineTable({ name: v.string(), orgId: v.optional(v.string()) }).index("by_orgId", ["orgId"]),
   users: defineTable({
     tokenIdentifier: v.string(),
-  })
+    orgIds: v.array(v.string()),
+    name: v.optional(v.string()),
+    image: v.optional(v.string()),
+  }).index('by_tokenIdentifier', ["tokenIdentifier"])
 });
--- convex/users.ts
+++ convex/users.ts
@@ -1,9 +1,49 @@
-import { v } from "convex/values";
-import { internalMutation } from "./_generated/server";
+import { ConvexError, v } from "convex/values";
+import {
+  MutationCtx,
+  QueryCtx,
+  internalMutation,
+  query,
+} from "./_generated/server";
+// import { roles } from "./schema";
+// import { hasAccessToOrg } from "./files";
+
+export async function getUser(
+    ctx: QueryCtx | MutationCtx,
+    tokenIdentifier: string
+  ) {
+    const user = await ctx.db
+      .query("users")
+      .withIndex("by_tokenIdentifier", (q) =>
+        q.eq("tokenIdentifier", tokenIdentifier)
+      )
+      .first();
+  
+    if (!user) {
+      throw new ConvexError("expected user to be defined");
+    }
+  
+    return user;
+}
 
 export const createUser = internalMutation({
-    args: { createUser: v.string()},
+    args: { tokenIdentifier: v.string(), name: v.string(), image: v.string() },
     async handler(ctx, args) {
-
+        await ctx.db.insert("users", { 
+            tokenIdentifier: args.tokenIdentifier, 
+            orgIds: [],
+            name: args.name,
+        });
     }
 })
+
+export const addOrgIdToUser = internalMutation({
+    args: { tokenIdentifier: v.string(), orgId: v.string(), role: roles },
+    async handler(ctx, args) {
+      const user = await getUser(ctx, args.tokenIdentifier);
+  
+      await ctx.db.patch(user._id, {
+        orgIds: [...user.orgIds, { orgId: args.orgId, role: args.role }],
+      });
+    },
+});
