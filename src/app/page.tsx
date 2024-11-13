e0ae46b5465fe65bbf42791ea4627e4c8e15e96b
Cedric <128242507+Cedrix-code@users.noreply.github.com>
2024-11-14 01:15:04 +0800

    wrapping up on dialog box

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import { useOrganization, useUser } from "@clerk/nextjs";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -23,24 +22,44 @@ import { useForm } from "react-hook-form"
 
 const formSchema = z.object({
   title: z.string().min(1).max(200),
-})
+  file: z.custom<FileList>((val) => val instanceof FileList, "Required").refine(
+    (files) => files.length > 0, `Required`)
+});
 
 export default function Home() {
   const organization= useOrganization();
   const user = useUser();
+  const generateUploadUrl = useMutation(api.files.generateUploadUrl);
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      title: "",
     },
   });
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values)
-  }
+  const fileRef = form.register("file");
 
-  let orgId = null;
+async function onSubmit(values: z.infer<typeof formSchema>) {
+  console.log(values)
+  console.log(values.file)
+  if (!orgId) return;
+  const postUrl = await generateUploadUrl();
+  
+  const result = await fetch(postUrl, {
+    method: "POST",
+    headers: { "Content-Type": values.file[0].type },
+    body: values.file[0],
+  });
+  const { storageId } = await result.json();
+
+  createFile({
+    name: values.title,
+    fileId: storageId,
+    orgId,
+  });
+}
+
+  let orgId = string | undefined = undefined;
   if (organization.isLoaded && user.isLoaded) {
     orgId = organization.organization?.id ?? user.user?.id;
   }
@@ -55,19 +74,14 @@ export default function Home() {
 
           <Dialog>
             <DialogTrigger asChild>
-            <Button onClick={() => {
-                if (!orgId) return;
-                createFile({
-                  name: "hello world",
-                  orgId
-                });
-            }}>
-            Upload File
-          </Button>
+              <Button onClick={() => {
+                }}>
+                Upload File
+              </Button>
             </DialogTrigger>
             <DialogContent>
               <DialogHeader>
-                <DialogTitle>Upload your file here</DialogTitle>
+                <DialogTitle className="mb-8">Upload your file here</DialogTitle>
                 <DialogDescription>
                 <Form {...form}>
                   <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
@@ -78,11 +92,21 @@ export default function Home() {
                         <FormItem>
                           <FormLabel>Title</FormLabel>
                           <FormControl>
-                            <Input placeholder="type title here" {...field} />
+                            <Input {...field} />
                           </FormControl>
-                          <FormDescription>
-                            The title of your file
-                          </FormDescription>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="file"
+                      render={() => (
+                        <FormItem>
+                          <FormLabel>File</FormLabel>
+                          <FormControl>
+                            <Input type="file" {...fileRef} />
+                          </FormControl>
                           <FormMessage />
                         </FormItem>
                       )}
